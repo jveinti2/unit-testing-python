f69c59dfed18aa737e9ad60b2b06517e1cc82f97
Jhonatan Estiven Becerra Pedrozo <jbecerrap95@gmail.com>
2024-12-27 07:20:44 -0500

    Add pytest configuration and initial tests for BankAccount class

---
+++ tests/test_pytest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from src.bank_account import BankAccount
+
+@pytest.mark.parametrize("ammount, expected", [(0, 1000), (-500, 1000), (500, 1500)])
+def test_deposit_vr_ammounts(ammount, expected):
+    account = BankAccount(balance=1000, log_file='test_account_1.txt')
+    new_balance = account.deposit(ammount)
+    assert new_balance == expected
+    
+
+def test_sum():
+    assert 1 + 2 == 3
+
