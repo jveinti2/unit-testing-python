733b570f418603e1e8bae941ae1b8325a0be614f
Jhonatan Estiven Becerra Pedrozo <jbecerrap95@gmail.com>
2024-12-27 07:24:59 -0500

    Remove obsolete test log file and update test for deposit amounts

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.bank_account import BankAccount
 
+
 @pytest.mark.parametrize("ammount, expected", [(0, 1000), (-500, 1000), (500, 1500)])
 def test_deposit_vr_ammounts(ammount, expected):
     account = BankAccount(balance=1000, log_file='test_account_1.txt')
